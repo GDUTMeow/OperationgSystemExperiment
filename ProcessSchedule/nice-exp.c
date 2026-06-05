e2b63bf605ed3a983ea2c1689bcb18f4de1ddfeb
GamerNoTitle <GamerNoTitle@outlook.com>
2026-06-05 17:22:46 +0800

    feat: 初始化仓库

---
+++ ProcessSchedule/nice-exp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+void *thread_fun(void *param)
+{
+    printf("thread pid:%d, tid:%lu\n", getpid(), pthread_self());
+    while (1)
+        ;
+    return NULL;
+}
+int main(void)
+{
+    pthread_t tid;
+    int ret;
+    printf("main pid:%d, tid:%lu\n", getpid(), pthread_self());
+    ret = pthread_create(&tid, NULL, thread_fun, NULL);
+    if (ret == -1)
+    {
+        perror("cannot create new thread");
+        return 1;
+    }
+    if (pthread_join(tid, NULL) != 0)
+    {
+        perror("call pthread_join function fail");
+        return 1;
+    }
+    return 0;
+}
